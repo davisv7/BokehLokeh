50fe00947939125bf46d37313ca5337896953b49
= <=>
2019-09-28 21:36:57 -0400

    sockets should work to allow 1 to 1 communication

--- client.py
+++ client.py
@@ -5,19 +5,40 @@ import sys
 import pickle
 import struct
 from main import Compressor
-cap=cv2.VideoCapture(0)
-clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-clientsocket.connect(('localhost',8089))
+from random import randint
 
-while True:
-    compressor = Compressor()
-    ret,frame=cap.read()
-    frame = compressor.compress_frame(frame)
-    # Serialize frame
-    data = pickle.dumps(frame)
 
-    # Send message length first
-    message_size = struct.pack("L", len(data)) ### CHANGED
+def main():
+    data_size = 1024
+    cap = cv2.VideoCapture(0)
 
-    # Then data
-    clientsocket.sendall(message_size + data)
+
+    try:
+        while True:
+            clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            clientsocket.connect(('localhost', 8089))
+            compressor = Compressor()
+            ret, frame = cap.read()
+            frame = compressor.compress_frame(frame)
+            # Serialize frame
+            data = pickle.dumps(frame)
+            # data = pickle.dumps([randint(0, 1000) for i in range(1000)])
+            # Send message length first
+            message_size = struct.pack("L", len(data))  ### CHANGED
+
+            # Then data
+            clientsocket.sendall(message_size + data)
+            frame_data = clientsocket.recv(data_size)
+            # Extract frame
+            frame = pickle.loads(frame_data)
+            # Display
+            cv2.imshow('frame', frame)
+            cv2.waitKey(1)
+            # return frame
+            # print("received frame!")
+            clientsocket.close()
+    except KeyboardInterrupt:
+        clientsocket.close()
+
+
+main()
--- server.py
+++ server.py
@@ -4,42 +4,70 @@ import struct
 
 import cv2
 
-HOST = ''
-PORT = 8089
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print('Socket created')
-
-s.bind((HOST, PORT))
-print('Socket bind complete')
-s.listen(10)
-print('Socket now listening')
-
-conn, addr = s.accept()
-
-data = b'' ### CHANGED
-payload_size = struct.calcsize("L") ### CHANGED
-
-while True:
+def get_data(conn):
+    data_size = 1024
+    data = b''  ### CHANGED
+    payload_size = struct.calcsize("L")  ### CHANGED
 
     # Retrieve message size
     while len(data) < payload_size:
-        data += conn.recv(4096)
+        data += conn.recv(data_size)
 
     packed_msg_size = data[:payload_size]
     data = data[payload_size:]
-    msg_size = struct.unpack("L", packed_msg_size)[0] ### CHANGED
+    msg_size = struct.unpack("L", packed_msg_size)[0]  ### CHANGED
 
     # Retrieve all data based on message size
     while len(data) < msg_size:
-        data += conn.recv(4096)
+        data += conn.recv(data_size)
 
     frame_data = data[:msg_size]
     data = data[msg_size:]
+    return data
 
-    # Extract frame
-    frame = pickle.loads(frame_data)
+    # # Extract frame
+    # frame = pickle.loads(frame_data)
+    # # Display
+    # # cv2.imshow('frame', frame)
+    # # cv2.waitKey(1)
+    # return frame
 
-    # Display
-    cv2.imshow('frame', frame)
-    cv2.waitKey(1)
+
+def manage_clients(clients):
+    if len(clients) != 2:
+        return
+    else:
+        frame1 = get_data(clients[0])
+        frame2 = get_data(clients[1])
+        clients[0].send(frame1)
+        clients[1].send(frame2)
+        print('frames sent')
+        clients[0].close()
+        clients[1].close()
+
+def main():
+    HOST = ''
+    PORT = 8089
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print('Socket created')
+
+    s.bind((HOST, PORT))
+    print('Socket bind complete')
+    s.listen(2)
+    print('Socket now listening')
+
+    clients = []
+    try:
+        while True:
+            print(clients)
+            if len(clients)<2:
+                conn, addr = s.accept()
+                clients.append(conn)
+            else:
+                manage_clients(clients)
+                clients = []
+    except KeyboardInterrupt:
+        s.close()
+main()
